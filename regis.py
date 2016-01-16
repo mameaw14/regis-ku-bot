e189ed5a6ee53fd095efb3eb671fa4fe53c95146
mameaw14 <pinyapornster@gmail.com>
2016-01-16 16:55:00 +0700

    connect to inst_regis.php

--- regis.py
+++ regis.py
@@ -25,3 +25,21 @@ s = s.findAll('tr')
 for i in range(1,len(s)-1):
 	ct = s[i].findAll('td')
 	print ct[1].contents[0] + '\t' + ct[2].contents[0]
+
+#_Student_Registration.php
+headers = {'Referer': 'https://std.regis.ku.ac.th/_Student_Registration.php', 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0'}
+regis = session.get('https://std.regis.ku.ac.th/_Student_Registration.php',headers=headers)
+soup = BeautifulSoup(regis.text,'lxml')
+t = soup.findAll('td',class_='txt_add')[4]
+t = t.findAll('input')
+payload = {}
+for i in t:
+	payload[i['name']] = i['value']
+code = raw_input('input subject code: ')
+payload['Cs_Code'] = code
+
+# =>inst_regis.php
+headers['Referer'] = 'https://std.regis.ku.ac.th/inst_regis.php'
+regis = session.post('https://std.regis.ku.ac.th/inst_regis.php',headers=headers,data=payload)
+print regis.text
+
